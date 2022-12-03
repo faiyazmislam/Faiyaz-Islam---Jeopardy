f025c599e89f8c9f7a913ac5c48779f91874285e
JenSal9159 <70230990+JenSal9159@users.noreply.github.com>
2022-12-03 14:39:06 -0500

    Update frmWinner.Designer.cs

--- 3309 - Term Project - Jeopardy/frmWinner.Designer.cs
+++ 3309 - Term Project - Jeopardy/frmWinner.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace _3309___Term_Project___Jeopardy
+namespace _3309___Term_Project___Jeopardy
 {
     partial class frmWinner
     {
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblWinner = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblWinner.Location = new System.Drawing.Point(321, 36);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(154, 36);
+            this.lblWinner.TabIndex = 0;
+            this.lblWinner.Text = "Winner(s):";
             // 
             // frmWinner
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblWinner);
             this.Name = "frmWinner";
             this.Text = "Winner";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblWinner;
     }
 }
